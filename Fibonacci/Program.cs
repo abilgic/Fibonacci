5e1b10f1e9a29d3aa877b77c98c6e983a6e36620
ahmet bilgic <ahmet_bilgic@yahoo.com>
2022-09-30 10:28:08 +0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+            ///
+            var result = Fibonacci(Convert.ToInt32(9));
+            Console.WriteLine($"Fibonacci : {result}");
+        }
+        static int Fibonacci(int n)
+        {
+            if (n == 0)
+                return 0;
+            if( n == 1)
+                return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
